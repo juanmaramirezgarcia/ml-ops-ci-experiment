09cf715d6b8caff4f69acdf3988e5fbb0fcff569
juanmaramirezgarcia <101336518+juanmaramirezgarcia@users.noreply.github.com>
2022-04-03 13:11:00 +0200

    Update model.py

--- model.py
+++ model.py
@@ -99,4 +99,4 @@ print(f'\nMean Absolute Error = {mae_1}, Mean Squared Error = {mse_1}.')
 
 # Write metrics to file
 with open('metrics.txt', 'w') as outfile:
-    outfile.write(f'\nMean Absolute Error = {mae_1}, Mean Squared Error = {mse_1}.')
+    outfile.write(f'\nRESULTS:\nMean Absolute Error = {mae_1}, Mean Squared Error = {mse_1}.')
